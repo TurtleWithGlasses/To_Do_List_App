dd9aa9ad1406c9ba8c89d657d4a88c9850b3002d
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-06-13 17:58:14 +0300

    improved the fucntions

--- to_do_list_app.py
+++ to_do_list_app.py
@@ -1,29 +1,31 @@
 to_do_file = "C:\\Users\\mhmts\\PycharmProjects\\To_Do_List_App\\todo.txt"
 
-def get_todos():
+def get_todos(to_do_file):
     with open(to_do_file, "r") as file:
         todos_local = file.readlines()
     return todos_local
+
+def write_todos(to_do_file, todos):
+    with open(to_do_file, "w") as file:
+        file.writelines(todos)
 
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = get_todos(to_do_file)
 
         todos.append(todo + "\n")
 
-        with open(to_do_file, "w") as file:
-            file.writelines(todos)
+        write_todos(to_do_file, todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = get_todos(to_do_file)
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -34,13 +36,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = get_todos(to_do_file)
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            with open(to_do_file, "w") as file:
-                file.writelines(todos)
+            write_todos(to_do_file, todos)
 
         except ValueError:
             print("Your command is not valid")
@@ -50,14 +51,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = get_todos(to_do_file)
 
             index = number-1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
 
-            with open(to_do_file, "w") as file:
-                file.writelines(todos)
+            write_todos(to_do_file, todos)
 
             print(f"Todo {todo_to_remove} is completed and removed from the list..")
 
