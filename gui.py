b5cecad4dae19f3290f0c34fdd17778c6ef53277
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-06-15 13:03:53 +0300

    changed main as cli, created new file as gui for interface

---
+++ gui.py
@@ -0,0 +1,10 @@
+import to_do_functions
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My App", layout=[[label],[input_box,add_button]])
+window.read()
+window.close()
