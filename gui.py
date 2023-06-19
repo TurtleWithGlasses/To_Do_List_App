ea61d26933b035e265e4fbb962e1a87e62dd6d62
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-06-19 15:35:51 +0300

    Add "add" and "complete" icons

--- gui.py
+++ gui.py
@@ -7,11 +7,12 @@ sg.theme("DarkTeal10")
 time_label = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=20,image_source="C:\\Users\\mhmts\\PycharmProjects\\To_Do_List_App\\add.png", mouseover_colors="gray",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=to_do_functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
-
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(size=20,image_source="C:\\Users\\mhmts\\PycharmProjects\\To_Do_List_App\\complete.png", mouseover_colors="gray",
+                       tooltip="Complete Todo", key="Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My App",
